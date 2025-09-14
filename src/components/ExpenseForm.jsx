ed9026b1e846925919b81bae4b54c3e78cf98a08
Sachin Rajbhar <officialsachinrajbhar@gmail.com>
2025-09-15 01:30:18 +0530

    added-unidirectional-state

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+export default function ExpenseForm({setExpenses}) {
+  // const [title, setTitle] = useState('')
+  // const [category, setCategory] = useState('')
+  // const [amount, setAmount] = useState('')
+  const [expense, setExpense] = useState({
+    title: '',
+    category: '',
+    amount: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // updating state in case of Controlled React Unidirectional Data Flow to update UI &   then set it to empty
+    setExpenses((prevState) => [...prevState, {...expense, id: crypto.randomUUID()},])
+    setExpense({
+        title: '',
+        category: '',
+        amount: '', 
+    })
+    // const expense = { ...getFormData(e.target), id: crypto.randomUUID() }
+    // setExpenses((prevState) => [...prevState, expense])
+    // e.target.reset()
+
+    // const expense = { title, category, amount, id: crypto.randomUUID() }
+    // setExpenses((prevState) => [...prevState, expense])
+    // setTitle('')
+    // setCategory('') 
+    // setAmount('')
+  }
+
+  // const getFormData = (form) => {
+  //   const formData = new FormData(form)
+  //   const data = {}
+  //   // Display the values
+  //   for (const [key, value] of formData.entries()) {
+  //     data[key] = value
+  //   }
+  //   return data
+  // }
+
+  return (
+    <form
+      className="expense-form"
+      onSubmit={handleSubmit}
+    >
+      <div className="input-container">
+        <label htmlFor="title">Title</label>
+        <input id="title" name='title' value={expense.title} onChange={(e) => setExpense((prevState) => ({...prevState, title: e.target.value}))} />
+      </div>
+      <div className="input-container">
+        <label htmlFor="category">Category</label>
+        <select id="category" name='category' value={expense.category} onChange={(e) => setExpense((prevState) => ({...prevState, category: e.target.value}))}>
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="Grocery">Grocery</option>
+          <option value="Clothes">Clothes</option>
+          <option value="Bills">Bills</option>
+          <option value="Education">Education</option>
+          <option value="Medicine">Medicine</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor="amount">Amount</label>
+        <input id="amount" name='amount' value={expense.amount} onChange={(e) => setExpense((prevState) => ({...prevState, amount: e.target.value}))} />
+      </div>
+      <button className="add-btn">Add</button>
+    </form>
+  )
+}
---
+++ src/expenseData.js
@@ -0,0 +1,27 @@
+export default [
+  // use js crypto default new Library instead if UUID npm library
+  {
+    id: crypto.randomUUID(),
+    title: 'Milk',
+    category: 'Grocery',
+    amount: 40,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Shirt',
+    category: 'Clothes',
+    amount: 600,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Vegetables',
+    category: 'Grocery',
+    amount: 100,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Electricity Bill',
+    category: 'Bills',
+    amount: 1100,
+  },
+]
