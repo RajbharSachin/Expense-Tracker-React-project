649f21042000c781cda91e811e45307b09aa7fb4
Sachin Rajbhar <officialsachinrajbhar@gmail.com>
2025-09-21 19:50:55 +0530

    add-useRef-to-avoid-render-but-not-good-practice

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,24 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-export default function ExpenseForm({setExpenses}) {
+export default function ExpenseForm({ setExpenses }) {
   // const [title, setTitle] = useState('')
   // const [category, setCategory] = useState('')
   // const [amount, setAmount] = useState('')
-  const [expense, setExpense] = useState({
-    title: '',
-    category: '',
-    amount: '',
-  })
+  // const [expense, setExpense] = useState({
+  //   title: '',
+  //   category: '',
+  //   amount: '',
+  // })
+
+  const myRef = useRef(0) // persists old refrence value   but not re-renders it
+
+  // to render first   then console 
+  // useEffect(() => {
+  //   console.log(myRef.current.value);
+  // })
+  // let myNum = 0
+
+
+  const titleRef = useRef()
+  const categoryRef = useRef()
+  const amountRef = useRef()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log({
+      title: titleRef.current.value,
+      category: categoryRef.current.value,
+      amount: amountRef.current.value,
+      id: crypto.randomUUID()
+    });
+
+
     // updating state in case of Controlled React Unidirectional Data Flow to update UI &   then set it to empty
-    setExpenses((prevState) => [...prevState, {...expense, id: crypto.randomUUID()},])
-    setExpense({
-        title: '',
-        category: '',
-        amount: '', 
-    })
+    setExpenses((prevState) => [
+      ...prevState,
+      {
+        title: titleRef.current.value,
+        category: categoryRef.current.value,
+        amount: amountRef.current.value,
+        id: crypto.randomUUID()
+      },
+    ])
+    // setExpense({
+    //   title: '',
+    //   category: '',
+    //   amount: '',
+    // })
     // const expense = { ...getFormData(e.target), id: crypto.randomUUID() }
     // setExpenses((prevState) => [...prevState, expense])
     // e.target.reset()
@@ -26,7 +55,7 @@ export default function ExpenseForm({setExpenses}) {
     // const expense = { title, category, amount, id: crypto.randomUUID() }
     // setExpenses((prevState) => [...prevState, expense])
     // setTitle('')
-    // setCategory('') 
+    // setCategory('')
     // setAmount('')
   }
 
@@ -41,32 +70,78 @@ export default function ExpenseForm({setExpenses}) {
   // }
 
   return (
-    <form
-      className="expense-form"
-      onSubmit={handleSubmit}
-    >
-      <div className="input-container">
-        <label htmlFor="title">Title</label>
-        <input id="title" name='title' value={expense.title} onChange={(e) => setExpense((prevState) => ({...prevState, title: e.target.value}))} />
-      </div>
-      <div className="input-container">
-        <label htmlFor="category">Category</label>
-        <select id="category" name='category' value={expense.category} onChange={(e) => setExpense((prevState) => ({...prevState, category: e.target.value}))}>
-          <option value="" hidden>
-            Select Category
-          </option>
-          <option value="Grocery">Grocery</option>
-          <option value="Clothes">Clothes</option>
-          <option value="Bills">Bills</option>
-          <option value="Education">Education</option>
-          <option value="Medicine">Medicine</option>
-        </select>
-      </div>
-      <div className="input-container">
-        <label htmlFor="amount">Amount</label>
-        <input id="amount" name='amount' value={expense.amount} onChange={(e) => setExpense((prevState) => ({...prevState, amount: e.target.value}))} />
-      </div>
-      <button className="add-btn">Add</button>
-    </form>
+    <>
+      <button
+        onClick={() => {
+          myRef.current = myRef.current + 1
+          // myNum = myNum + 1
+          console.log('myRef = ', myRef.current)
+          // console.log('myNum = ', myNum)
+        }}
+        ref={myRef}
+      >
+        Click
+      </button>
+      {/* <h1>
+        myRef = {myRef.current}, myNum = {myNum}
+      </h1> */}
+      <form className="expense-form" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            name="title"
+            // value={expense.title}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     title: e.target.value,
+            //   }))
+            // }
+            ref={titleRef}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="category">Category</label>
+          <select
+            id="category"
+            name="category"
+            // value={expense.category}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     category: e.target.value,
+            //   }))
+            // }
+            ref={categoryRef}
+          >
+            <option value="" hidden>
+              Select Category
+            </option>
+            <option value="Grocery">Grocery</option>
+            <option value="Clothes">Clothes</option>
+            <option value="Bills">Bills</option>
+            <option value="Education">Education</option>
+            <option value="Medicine">Medicine</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            name="amount"
+            // value={expense.amount}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     amount: e.target.value,
+            //   }))
+            // }
+            ref={amountRef}
+          />
+        </div>
+        <button className="add-btn">Add</button>
+      </form>
+    </>
   )
 }
